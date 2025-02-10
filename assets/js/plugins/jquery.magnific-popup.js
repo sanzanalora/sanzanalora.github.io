fc03edc40a2181bceec628c8ee52782449c6c483
Sanzana Karim Lora <46655480+sanzanalora@users.noreply.github.com>
2025-02-10 19:28:28 +0600

    Update jquery.magnific-popup.js

--- assets/js/plugins/jquery.magnific-popup.js
+++ assets/js/plugins/jquery.magnific-popup.js
@@ -1,4 +1,4 @@
-/*! Magnific Popup - v0.9.9 - 2013-12-27
+/*! Magnific Popup - v0.9.9 - 2013-12-27 
 * http://dimsemenov.com/plugins/magnific-popup/
 * Copyright (c) 2013 Dmitry Semenov; */
 ;(function($) {
